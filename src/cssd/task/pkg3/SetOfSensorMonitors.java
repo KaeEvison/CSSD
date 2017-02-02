2caa19c63f43d94d3dd0454a55d695c405b532f8
thoma <thoma@DESKTOP-F6R1HGQ>
2017-02-02 15:34:32 +0000

    SetOfSensorMonitors class

---
+++ src/cssd/task/pkg3/SetOfSensorMonitors.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cssd.task.pkg3;
+
+import java.util.ArrayList;
+import java.io.Serializable;
+
+/**
+ *
+ * @author trkirk
+ */
+public class SetOfSensorMonitors extends ArrayList<SensorMonitor> implements Serializable
+{
+    public SetOfSensorMonitors(){
+        super();
+    }
+    
+    public void addSensorMonitor(SensorMonitor sensorMonitor){
+        super.add(sensorMonitor);
+    }
+    
+    public void removeSensorMonitor(int index){
+        super.remove(index);
+    }
+}
